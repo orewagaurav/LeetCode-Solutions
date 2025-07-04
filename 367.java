536ff25bfac0fb21b5447b7f97c19db9b63c753e
orewagaurav <gauravkumar47967@gmail.com>
2025-07-04 23:55:11 +0530

    367

---
+++ 367.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long i = 1;
+        long j = num;
+        while(i <= j){
+            long mid = (i+j)/2;
+            if(mid * mid == num){
+                return true;
+            }
+            else if(mid * mid < num){
+                i = mid +1;
+            }
+            else{
+                j = mid-1;
+            }
+        }
+        return false;
+    }
+}
