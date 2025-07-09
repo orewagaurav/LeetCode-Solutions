e17cb636c7d31e8cd945b40fd28b03934b7187f2
orewagaurav <gauravkumar47967@gmail.com>
2025-07-09 22:04:07 +0530

    349

---
+++ 349.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for(int i:nums1){
+            set.add(i);
+        }
+        for(int i:nums2){
+            if(set.contains(i)){
+                set2.add(i);
+            }
+        }
+        int []result = new int[set2.size()];
+        int index = 0;
+        for(int i:set2){
+            result[index++] = i;
+        }
+        return result;
+
+    }
+
+}
