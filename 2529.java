041ed22a50bfa7cfbab4ec8cf77606462d720730
orewagaurav <gauravkumar47967@gmail.com>
2025-07-10 19:43:09 +0530

    2529

---
+++ 2529.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int neg=0;
+        int pos = 0;
+        for (int  i:nums){
+            if(i>0) pos++;
+            else if(i<0) neg++;
+        }
+        return Math.max(neg,pos);
+    }
+}
