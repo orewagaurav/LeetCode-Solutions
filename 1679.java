0cba9e0d5d3874548d3dc4f1c30ba7a377fe6d1a
orewagaurav <gauravkumar47967@gmail.com>
2025-12-17 19:00:53 +0530

    1679

---
+++ 1679.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int count  = 0;
+        for(int i : nums){
+            int target = k - i;
+            if(map.containsKey(target) && map.get(target) > 0){
+                count++;
+                map.put(target,map.get(target)-1);
+            }else{
+                map.put(i,map.getOrDefault(i,0)+1);
+            }
+        }
+        return count;
+    }
+}
