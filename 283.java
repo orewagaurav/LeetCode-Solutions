1379d43cf698b2641c360aadd8d5393b301b0e67
orewagaurav <gauravkumar47967@gmail.com>
2025-12-17 11:03:56 +0530

    283

--- 283.java
+++ 283.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0;
-        for(int right=0;right<nums.length;right++){
-            if(nums[right]!=0){
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left]=temp;
-                left++;
+        int len = nums.length;
+        int j = 0;
+        for(int i=0;i<len;i++){
+            if(nums[i] != 0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
             }
         }
     }
