a4537d1005235c2c93074a294c5993937124b5e7
orewagaurav <gauravkumar47967@gmail.com>
2026-01-08 23:55:03 +0530

    101

---
+++ 101.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean mirror(TreeNode p ,TreeNode q){
+        if(p == null && q == null)return true;
+        if(p == null|| q == null)return false;
+        if(p.val != q.val) return false;
+        return mirror(p.left,q.right) && mirror(p.right,q.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return mirror(root.left,root.right);    
+    }
+}
