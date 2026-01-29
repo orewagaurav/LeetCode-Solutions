b35f99c49f505e95b2bc831735c718040e989b75
orewagaurav <gauravkumar47967@gmail.com>
2026-01-29 23:29:01 +0530

    735.java

---
+++ 735.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        int n = asteroids.length;
+        for(int i=0;i<n;i++){
+            if(stack.isEmpty() || asteroids[i] > 0) stack.push(asteroids[i]);
+            else{
+                int val = stack.peek();
+                int bomb  = Math.abs(asteroids[i]);
+                if(val < 0){
+                    stack.push(asteroids[i]);
+                }else{
+                    while(val <= bomb){
+                        if(val == bomb){
+                            stack.pop();
+                            break;
+                        }else if(val < 0){
+                            stack.push(asteroids[i]);
+                            break;
+                        }
+                        stack.pop();
+                        if(!stack.isEmpty()){
+                            val = stack.peek();
+                        }else{
+                            stack.push(asteroids[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int[] nums = new int[stack.size()];
+        int index = 0;
+        for(int num : stack){
+            nums[index++] = num;
+        }
+        return nums;
+    }
+}
